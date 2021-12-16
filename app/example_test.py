0924e7c6d7fae427808230a399c1d6a24d41f543
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2021-12-16 14:52:12 +0100

    [ADD] Docker

--- app/example_test.py
+++ app/example_test.py
@@ -1,7 +1,7 @@
 import requests
 
 # Simple test script - execute when the app is running
-url = "http://127.0.0.1:5000"
+url = "http://one-to-eth-ddo5p.ondigitalocean.app"
 one_address = "one1cwsf0lrq0hzphqa79q8pwrn6pnzzhwej4tqen3"
-res = requests.post(url, params={"address": one_address}).json()
+res = requests.post(url, params={"address": one_address}).text
 print(res)
