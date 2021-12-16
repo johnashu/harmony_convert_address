511df156628c66df35460f33f7f2aef29eb2fee4
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2021-12-16 14:37:15 +0100

    [ADD] Docker

--- example_test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-# Simple test script - execute when the app is running
-url = "http://127.0.0.1:5000"
-one_address = "one1cwsf0lrq0hzphqa79q8pwrn6pnzzhwej4tqen3"
-res = requests.post(url, params={"address": one_address}).json()
-print(res)
